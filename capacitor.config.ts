51aea8f8ea02a01db41a6bf501f2d83538c2b5d9
udohdivine07-spec <udohdivine07@gmail.com>
2026-08-23 14:45:11 +0100

    Fix Bookly to load inside the Android app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bookly.app',
   appName: 'Bookly',
-  webDir: 'www',
-  server: {
-    url: 'https://bookly-edyd.hatchable.site',
-    cleartext: false
-  }
+  webDir: 'www'
 };
 
 export default config;
